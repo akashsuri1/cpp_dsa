237ff74289ec7966806975b26ac31c3a69b9657d
akash1979 <akashsuri1979@gmail.com>
2023-03-07 10:07:41 +0530

    it is amazing

--- quaratic.c
+++ quaratic.c
@@ -1,16 +1,31 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    int d=b*b-4*a*c;
-    if(d>0){
-        printf("root 1= %f and root 2= %f\n",(-b+sqrt(d))/(2*a),(-b-sqrt(d))/(2*a));
-    }else if(d==0){
-        printf("root 1= %f and root 2= %f \n",-b*1.0/(2*a),-b*1.0/(2*a));
-    }else{
-        printf("root 1= %f + i %f and root 2= %f - i %f \n",(-b*1.0/(2*a)),sqrt(-d)/(2*a),(-b*1.0/(2*a)),sqrt(-d)/(2*a));
-    }   
+    int n;
+    printf("enter the size of the array: ");
+    scanf("%d",&n);
+    int array[n];
+    printf("enter the elements of the array : ");
+    int sum=0;
+    for(int i=0;i<n;i++){
+        scanf("%d",&array[i]);
+        sum+=array[i];
+    }
+    int min,max;
+    min=max=array[0];
+    for(int i=1;i<n;i++){
+        if(array[i]>max){
+            max=array[i];
+        }
+        if(array[i]<min){
+            min=array[i];
+        }
+        printf("%d ",array[i]);
+    }
+    printf("\n");
+    printf("max=%d and min=%d\nsum=%d\n",max,min,sum);
     return 0;
+    
+
 
 }
