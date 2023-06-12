888e2b8fe1700f8034d6b9a8494c8c756f56a8b0
akash1979 <akashsuri1979@gmail.com>
2023-06-12 10:50:01 +0530

    completed operators

---
+++ c++_dsa/operators/logicaloperator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=3;
+    cout<<((a>b)&&(b>2))<<endl;
+    cout<<((a!=5)||(!(b>4)))<<endl;
+    cout<<(!(a>b))<<endl;
+    //so these are logical operator which are used to check multiple condition together thus making it easy to write logics
+    //these with the relational operator are used frequently in the conditional and lopping statments to make brillant working codes
+
+}
